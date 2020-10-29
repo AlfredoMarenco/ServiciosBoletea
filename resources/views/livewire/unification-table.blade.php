2c489825141b8b3194112826ba7d4f3255fc123b
marencoalf <alfredomarenco@boletea.com>
2020-10-29 17:34:08 -0600

    Cambiamos nombre a la columna accion

--- resources/views/livewire/unification-table.blade.php
+++ resources/views/livewire/unification-table.blade.php
@@ -38,7 +38,7 @@
                                         </th>
                                         <th
                                             class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                            Fecha de la unificacion
+                                            Accion
                                         </th>
                                     </tr>
                                 </thead>
