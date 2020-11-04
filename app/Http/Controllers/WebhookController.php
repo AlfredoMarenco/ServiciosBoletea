5215cf4711350ea891009a5353c2049df262bd7c
marencoalf <alfredomarenco@boletea.com>
2020-11-04 12:01:48 -0600

    Integracion de funciones nuevas

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use Openpay;
 
 class WebhookController extends Controller
 {
-    public function handle(Request $request)
+    public function webhook()
     {
-        Log::info($request);
-
-        return json_decode($request);
+        $openpay = Openpay::getInstance('m4gx48zqyw8xs4en1z1u', 'sk_d70ffc17846544e39488869d11fac3dc');
+        $webhook = array(
+            'url' => 'https://commercelaravel.com/openpay/webhook',
+            'event_types' => array(
+                'charge.refunded',
+                'charge.failed',
+                'charge.cancelled',
+                'charge.created',
+                'chargeback.accepted'
+            )
+        );
+        $webhook = $openpay->webhooks->add($webhook);
     }
 }
